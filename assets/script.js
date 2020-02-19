76bf47854fb4d1b5ebef7023c451ad68a50519f0
Shawn Wright <volushawn@gmail.com>
2020-02-19 00:24:03 -0600

    Random cocktail API request is functioning, and starting to parse out the response into usable pieces.

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+// using Cocktail DB api to request a random cocktail:
+
+let randomCocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php?api-key=1";
+
+//get random cocktail
+$.ajax({
+    url: randomCocktailURL,
+    method: "GET"
+  })
+    .then(function(response) {
+        console.log(response.drinks[0]);
+        //destructure the response object to discrete variables to use or display to the user
+        ({idDrink, strDrink: name, strInstructions: instructions, strDrinkThumb} = response.drinks[0]);
+        console.log(idDrink, name, instructions);
+    });
+
+    //@@todo - response object has 15 ingredient properties and 15 corresponding measurement properties. Need to write a function to pair these up and omit all 'null' values
+
