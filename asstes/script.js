bc7831737b04359510ca282436a752cb504a2ed4
Alberto Ruiz <alberto.ruiz.iii@gmail.com>
2020-02-19 10:39:50 -0600

    skeleton is good to go

---
+++ asstes/script.js
@@ -0,0 +1,11 @@
+alert("this is linked in a sting")
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+
+// $(document).ready(function(){
+//     $('.sidenav').sidenav();
+//   });
